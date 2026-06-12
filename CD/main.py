cd03733bf3c1751031d8815d3459530f44565600
Riszav <riszav.01@gmail.com>
2026-06-12 19:17:10 +0600

    added code day theme

--- CD/main.py
+++ CD/main.py
@@ -1,40 +1,29 @@
-# height = int(input("Введите высоту ёлки: "))
+database = []
 
-# for i in range(1, height + 1):
-#     print(" " * (height - i) + "*" * (2 * i - 1))
+while True:
+    print("1. Добавить отзыв")
+    print("2. Показать все отзывы")
+    print("3. Выйти")
+    choice = input("Выберите действие (1, 2, 3): ")
 
-# print(" " * (height - 1) + "|")
-
-# levels = int(input("Сколько ярусов у ёлки: "))
-
-# for level in range(1, levels + 1):
-#     for i in range(1, level * 3):
-#         spaces = (levels * 3) - i
-#         print(" " * spaces + "*" * (2 * i - 1))
-
-# print(" " * (levels * 3 - 1) + "|")
-
-
-import random
-
-# Цвета ANSI
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-RED = "\033[31m"
-RESET = "\033[0m"
-
-height = int(input("Введите высоту ёлки: "))
-
-for i in range(1, height + 1):
-    line = ""
-    for j in range(2 * i - 1):
-        # Рандомно выбираем: ёлка или игрушка
-        if random.random() < 0.15:  # ~15% шанс на игрушку
-            line += random.choice([RED + "o" + RESET, YELLOW + "o" + RESET])
+    if choice == "1":
+        name = input("Введите ваше имя: ")
+        review = input("Введите ваш отзыв: ")
+        stars = input("Введите количество звезд (1-5): ")
+        database.append({"name": name, "review": review, "stars": stars})
+        print("Отзыв добавлен!")
+    elif choice == "2":
+        if not database:
+            print("Нет отзывов для отображения.")
         else:
-            line += GREEN + "*" + RESET
-
-    print(" " * (height - i) + line)
-
-# Ствол
-print(" " * (height - 1) + YELLOW + "|" + RESET)
+            for i, v in enumerate(database, start=1):
+                print(f"{i}. {v['name']} - {v['review']} ({v['stars']} звезд)")
+        # print(database)
+        # for review in database:
+        #     print(f"{review['name']} - {review['review']} ({review['stars']} звезд)")
+    elif choice == "3":
+        print("Выход из программы. До свидания!")
+        break
+    else:
+        print("Неверный выбор. Пожалуйста, выберите 1, 2 или 3.")
+    print("\n")
